2f631cd06931c115cc56ce60b5e3b85ccb70527b
nafisfaiyaz-012 <contact.nafisfaiyaz@gmail.com>
2025-08-23 15:41:03 +0600

    cashout and transfer done with toggling feature

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
+// event on add money button
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
-
     const bankInput = document.getElementById("bank-input").value;
     const accountNumberInput = document.getElementById(
       "account-number-input"
@@ -29,28 +29,90 @@ document
     addMoneyForm.reset();
 
     //extra added a new message after the money add
-    /* 
-    addMoneyForm.classList.add("hidden");
+    document.getElementById("add-money-message").style.display = "block";
+    document.getElementById("add-money-section").style.display = "none";
 
-    const addMoneySection = document.getElementById("add-money-section");
-    const moneyAddedMessage = document.createElement("div");
-    moneyAddedMessage.innerHTML = `
-    <i class="fa-solid fa-check"></i>
-    <div>
-    <p>Money Added : ${addAmountInput}</p>
-    <p>Updated Balance : ${updatedBalance}</p>
-    </div>             
-    `;
-    moneyAddedMessage.classList.add(
-      "bg-white",
-      "p-8",
-      "rounded-3xl",
-      "flex",
-      "justify-center",
-      "items-center",
-      "gap-4"
-    );
-    addMoneySection.appendChild(moneyAddedMessage); 
-    
-    */
+    //add money msg update
+    document.getElementById("money-added").innerText =
+      "Money Added : " + addAmountInput;
+    document.getElementById("updated-balance").innerText =
+      "Updated Balance : " + updatedBalance;
   });
+//add money div event
+document.getElementById("add-money-div").addEventListener("click", function () {
+  document.getElementById("add-money-section").style.display = "block";
+  document.getElementById("cashout-section").style.display = "none";
+  document.getElementById("cashout-message").style.display = "none";
+  document.getElementById("transfer-money-section").style.display = "none";
+  document.getElementById("transfer-msg").style.display = "none";
+});
+//cashout div event
+document.getElementById("cashout-div").addEventListener("click", function () {
+  document.getElementById("cashout-section").style.display = "block";
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("add-money-message").style.display = "none";
+  document.getElementById("transfer-money-section").style.display = "none";
+  document.getElementById("transfer-msg").style.display = "none";
+});
+//withdraw button event
+document
+  .getElementById("btn-withdraw")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const withdrawAmount = parseInt(
+      document.getElementById("withdraw-amount").value
+    );
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+    const balanceAfterWithdraw = availableBalance - withdrawAmount;
+    console.log(balanceAfterWithdraw);
+    document.getElementById("available-balance").innerText =
+      balanceAfterWithdraw;
+    document.getElementById("cashout-form").reset();
+
+    //cashout message
+    document.getElementById("cashout-message").style.display = "block";
+    document.getElementById("cashout-section").style.display = "none";
+
+    //cashout message update
+    document.getElementById("cashout-money").innerText =
+      "CashOut Money : " + withdrawAmount;
+    document.getElementById("updated-balance-after-cashout").innerText =
+      "Updated Balance : " + balanceAfterWithdraw;
+  });
+// send now button event
+document
+  .getElementById("btn-send-now")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const transferAmount = parseInt(
+      document.getElementById("transfer-amount").value
+    );
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+    const balanceAfterTransfer = availableBalance - transferAmount;
+    document.getElementById("available-balance").innerText =
+      balanceAfterTransfer;
+    document.getElementById("transfer-money-form").reset();
+
+    //vanishing tranfer money form
+    document.getElementById("transfer-money-section").style.display = "none";
+    document.getElementById("transfer-msg").style.display = "block";
+
+    //transfer msg update
+    document.getElementById("trasnferred-money").innerText =
+      "Transferred Money : " + transferAmount;
+    document.getElementById("balance-after-transfer").innerText =
+      "Balance after transfer: " + balanceAfterTransfer;
+    console.log(balanceAfterTransfer);
+  });
+// transfer money card event
+document.getElementById("transfer-div").addEventListener("click", function () {
+  document.getElementById("transfer-money-section").style.display = "block";
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("cashout-section").style.display = "none";
+  document.getElementById("add-money-message").style.display = "none";
+  document.getElementById("transfer-msg").style.display = "none";
+});
